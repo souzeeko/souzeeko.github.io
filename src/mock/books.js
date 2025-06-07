deae1d0f4530ffa21cbdbae35035123733b28bec
PeePeePooPooGuy <125928785+souzeeko@users.noreply.github.com>
2025-06-07 06:32:57 +0300

    Delete src directory

--- src/mock/books.js
+++
@@ -1,7 +0,0 @@
-import { generateID } from '../utils.js';
-
-export const initialBooks = [
-  { id: generateID(), title: '1984', author: 'Джордж Оруэл', genre: 'Художественная' },
-  { id: generateID(), title: 'Цветы для Элджернона', author: 'Дэниэл Киз', genre: 'Фантастика' },
-  { id: generateID(), title: '451 градус по Фаренгейту', author: 'Рэй Брэдбери', genre: 'Художественная' }
-];
--- src/utils.js
+++
@@ -1,1 +0,0 @@
-export const generateID = () => Date.now() + Math.floor(Math.random() * 1000);
